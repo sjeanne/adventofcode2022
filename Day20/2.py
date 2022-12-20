7f6880702a2d437e10250188b079445b7454212e
loktor <loktor@gmail.com>
2022-12-20 09:56:22 +0100

    Day 20

---
+++ Day20/2.py
@@ -0,0 +1,40 @@
+import functools
+
+
+def moveElement(index, elementList):
+    element = elementList[index]
+    elementList = elementList[0:index] + elementList[index+1:]
+    elementList.insert((index+element["v"])%(len(elementList)), element)
+    return elementList
+
+def findIndexByPos(pos, elementList):
+    for i in range(0,len(elementList)):
+        if elementList[i]["p"] == pos:
+            return i
+
+def findIndexByValue(value, elementList):
+    for i in range(0,len(elementList)):
+        if elementList[i]["v"] == value:
+            return i    
+
+
+data = open('Day20/data.txt', 'r')
+
+buffer = []
+
+while True:
+
+    line = data.readline().strip()
+    if not line:
+        break
+    pass
+    buffer.append({"v":int(line)*811589153,"p":len(buffer)})
+
+for _ in range(0,10):
+    for i in range(0, len(buffer)):
+        index = findIndexByPos(i,buffer)
+        buffer = moveElement(index, buffer)
+
+
+indexZero = findIndexByValue(0,buffer)
+print(buffer[(indexZero+1000) % len(buffer)]["v"] + buffer[(indexZero+2000) % len(buffer)]["v"] + buffer[(indexZero+3000) % len(buffer)]["v"])
